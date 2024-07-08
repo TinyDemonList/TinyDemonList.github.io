83454fb3d495351b40cb625921fbc5fc1ca737bf
TinyDemonList <scratchmangd@gmail.com>
2024-07-08 12:19:50 -0700

    testing

--- JS/mainlist.js
+++ JS/mainlist.js
@@ -68,6 +68,7 @@ function appendData(data) {
       if(i == 0){
         victor.innerHTML = `
         <h6>ID: ${level.id}</h6>
+        <h6>Creator Points: ${level.creatorpoints}</h6>
           <h5>Completions<br></h5>
           <h6> ${level.list[i].name} - <a href = "${level.list[i].link}" target = "_blank">${level.list[i].link}</h6>
         `;
